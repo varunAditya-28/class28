948e82d3e404e277d8173fcdd017c5843f4c669e
varunAditya-28 <varunaditya.pds@gmail.com>
2021-06-01 12:55:48 +0530

    slingshot

---
+++ slingshot.js
@@ -0,0 +1,26 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.slingshot = Constraint.create(options);
+        World.add(world, this.slingshot);
+    }
+
+    display(){
+    if(this.slingshot.bodyA!=null){
+        var pointA = this.slingshot.bodyA.position;
+        var pointB = this.slingshot.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+     }
+   
+    fly(){
+    this .slingshot.bodyA=null
+    }
+
+}
